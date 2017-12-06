6c36d1dc57dc9596e5f0305ab6c14e7143ba6f76
Dgame <rswhite4@googlemail.com>
2017-12-06 17:43:45 +0100

    CTor Inheritance nutzen

--- src/JSendResponse.php
+++ src/JSendResponse.php
@@ -11,17 +11,6 @@ use BadMethodCallException;
 final class JSendResponse extends AbstractJSendResponse
 {
     /**
-     * JSendResponse constructor.
-     *
-     * @param StatusInterface $status
-     * @param array|null      $data
-     */
-    public function __construct(StatusInterface $status, array $data = null)
-    {
-        parent::__construct($status, $data);
-    }
-
-    /**
      * @return JSendErrorResponseInterface
      */
     public function getError(): JSendErrorResponseInterface
